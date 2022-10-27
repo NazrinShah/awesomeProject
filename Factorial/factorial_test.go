21d1d0eeef380b11e440edf6120505fc0f3a7b46
Nazrin Shah <nazrinshah.as@gmail.com>
2022-10-27 11:20:05 +0800

    add tests

--- Factorial/factorial_test.go
+++ Factorial/factorial_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
-func Test_Factorial_0(t *testing.T) {
+func Test_Factorial_Is_Zero(t *testing.T) {
 	assert.Equal(t, 1, factorial(0))
+}
+
+func Test_Factorial_Is_Negative(t *testing.T) {
+	assert.Equal(t, 1, factorial(-1))
+}
+
+func Test_Factorial_Is_Three(t *testing.T) {
+	assert.Equal(t, 6, factorial(3))
 }
