6994175c2c3dab5322ee0380d38b4b2102773333
Nazrin Shah <nazrinshah.as@gmail.com>
2022-11-03 13:11:53 +0800

    update tests

--- DataStructures/stack_test.go
+++ DataStructures/stack_test.go
@@ -11,11 +11,41 @@ func Test_stack_insert_001(t *testing.T) {
 	s.Push(1)
 	v, _ := s.Peek()
 	assert.Equal(t, 1, v)
+	assert.Equal(t, 1, s.Count())
+}
+
+func Test_stack_insert_002(t *testing.T) {
+	s := pkg.Stack{}
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	v, _ := s.Peek()
+	assert.Equal(t, 3, v)
+	assert.Equal(t, 3, s.Count())
 }
 
 func Test_stack_pop_001(t *testing.T) {
 	s := pkg.Stack{}
 	s.Push(1)
-	v, _ := s.Pop()
+	v, err := s.Pop()
+	assert.Nil(t, err)
 	assert.Equal(t, 1, v)
+	assert.Equal(t, 0, s.Count())
+}
+
+func Test_stack_pop_002(t *testing.T) {
+	s := pkg.Stack{}
+	s.Push(1)
+	s.Push(2)
+	v, err := s.Pop()
+	assert.Nil(t, err)
+	assert.Equal(t, 2, v)
+	assert.Equal(t, 1, s.Count())
+}
+
+func Test_stack_pop_empty(t *testing.T) {
+	s := pkg.Stack{}
+	_, err := s.Pop()
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, s.Count())
 }
